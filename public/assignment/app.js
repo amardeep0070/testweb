856b29a0c262a3cce5f73c03c7bb32a70ec96fbc
amardeep0070 <amardeep007-@gmail.com>
2016-11-18 11:09:17 -0500

    s

--- public/assignment/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Amardeep on 08/10/16.
- */
-//to avoid namespace duplication and clutter we use this.
-//Best Practice.
-(function () {
-    angular.module("WebAppMaker",["ngRoute","wamDirectives"]);
-})();
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.set('port', (process.env.PORT || 5000));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-require ("./test/app.js")(app);
-require("./assignment/app")(app);
+//require ("./test/app.js")(app);
+//require("./assignment/app")(app);
 //var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 //var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
